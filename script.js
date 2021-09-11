99605a6c718e103ed71a592d9dd6207bfe330fa0
Pedro <pedro.oficial1969@gmail.com>
2021-09-11 13:16:41 -0300

    adiciona atualizaçao do requisito 4

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function createPixels() {
   const bord = document.getElementById('pixel-board');
   for (let index = 0; index < 5; index += 1) {
-    let inline = document.createElement('div');
+    const inline = document.createElement('div');
     for (let ind = 0; ind < 5; ind += 1) {
-      let Pixel = document.createElement('div');
+      const Pixel = document.createElement('div');
       Pixel.classList.add('pixel');
       inline.appendChild(Pixel);
     }
-    bord.appendChild(inline)
+    bord.appendChild(inline);
   }
 }
-createPixels()
+createPixels();
